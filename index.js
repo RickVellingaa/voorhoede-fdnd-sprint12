0a244667796db7e9d3d6c59e23213506b3995355
RickVellingaa <rickvellinga2408@gmail.com>
2023-06-12 00:00:53 +0200

    Data request met Axios

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express'
+import express, { response } from 'express'
+import axios from 'axios'
 
-// Maak een nieuwe express app aan
 const app = express()
 
 // Stel ejs in als template engine en geef de 'views' map door
@@ -10,10 +10,45 @@ app.set('views', './views')
 // Gebruik de map 'public' voor statische resources
 app.use(express.static('public'))
 
-// Maak een route voor de index
-app.get('/', function (req, res) {
-  res.render('index')
-})
+
+app.get('/', async (req, res) => {
+  try {
+    const query = `
+    query AllBlogPosts {
+      allBlogPosts {
+        title
+        authors {
+          image {
+            url
+          }
+          name
+        }
+        publishDate
+      }
+    }
+    `;
+    
+    const variables = {
+      query: ''
+    };
+
+    const response = await axios.post('https://graphql.datocms.com/', JSON.stringify({
+      query,
+      variables
+    }), {
+      headers: {
+        Authorization: '10a0ae10c2d6418c1acd4346de9329'
+      }
+    });
+
+    const data = response.data.data;
+    console.log(data);
+
+    res.render('index', { data });
+  } catch (error) {
+
+  }
+});
 
 // Stel het poortnummer in waar express op gaat luisteren
 app.set('port', process.env.PORT || 8000)
