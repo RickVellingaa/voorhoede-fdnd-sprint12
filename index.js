a7367f133b2a354d628526cd7ccc1a92389c795a
RickVellingaa <rickvellinga2408@gmail.com>
2023-06-15 18:07:01 +0200

    Nieuw design
    Nieuw design op basis van de zero state deeltaak en de opdrachtgever feedback

--- index.js
+++ index.js
@@ -34,30 +34,30 @@ server.get("/", (request, response) => {
 })
 
 // Stel het poortnummer in en start express
-server.set("port", process.env.PORT || 8000)
+server.set("port", process.env.PORT || 2000)
 server.listen(server.get("port"), function () {
   console.log(`Application started on http://localhost:${server.get("port")}`)
 })
 
 // Functie GraphQL ophalen
 async function graphQLRequest(gql = "", variables = {}) {
-    return await fetch("https://graphql.datocms.com", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": '10a0ae10c2d6418c1acd4346de9329',
-        },
-        body: JSON.stringify({
-            query: gql,
-            variables,
-        }),
-    })
-        .then(async response => {
-          const body = await response.json()
-          if (body.errors) {
-            throw new Error(JSON.stringify(body.errors, null, 2))
-          }
-          return body
-        })
-        .catch(error => console.log(error))
+  return await fetch("https://graphql.datocms.com", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Authorization": process.env.GRAPHQL_KEY,
+      },
+      body: JSON.stringify({
+          query: gql,
+          variables,
+      }),
+  })
+      .then(async response => {
+        const body = await response.json()
+        if (body.errors) {
+          throw new Error(JSON.stringify(body.errors, null, 2))
+        }
+        return body
+      })
+      .catch(error => console.log(error))
 }
